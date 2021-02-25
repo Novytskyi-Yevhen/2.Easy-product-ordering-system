2f55b52d67dbc76cb81efa1398270471f0489480
alaketych <alaketych@ukr.net>
2021-02-25 13:22:13 +0200

    add action for task4

--- ViewTask/Controllers/TasksController.cs
+++ ViewTask/Controllers/TasksController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using ViewTask.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,31 +10,51 @@ namespace ViewTask.Controllers
 {
     public class TasksController : Controller
     {
-        public IActionResult Index() {
+        public IActionResult Index() 
+        {
             return View();
         }
 
-        public IActionResult Greetings() {
+        public IActionResult Greetings() 
+        {
             return View();
         }
 
-        public IActionResult Products() {
+        public IActionResult Products() 
+        {
             return View();
         }
 
-        public IActionResult ProductInfo() {
+        public IActionResult ProductInfo() 
+        {
+			Product[] products = new Product[]
+			{
+				new Product("Bread", 10),
+				new Product("Mil", 11),
+				new Product("Cheese", 140),
+				new Product("Sausage", 110),
+				new Product("Potato", 7),
+				new Product("Banana", 23),
+				new Product("Tomato", 25),
+				new Product("Candy", 75),
+			};
+
+
+            return View("Products", products);
+        }
+
+        public IActionResult SuperMarkets() 
+        {
             return View();
         }
 
-        public IActionResult SuperMarkets() {
+        public IActionResult ShoppingList() 
+        {
             return View();
         }
 
-        public IActionResult ShoppingList() {
-            return View();
-        }
-
-        public IActionResult ShoppingCart() {
+        public IActionResult ShoppingCart() 
+        {
             return View();
         }
     }
