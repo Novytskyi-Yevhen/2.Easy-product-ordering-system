7a495119e408fb116449f77391145636293ace46
Евгений Новицкий <serverdog1@gmail.com>
2021-02-25 20:45:42 +0200

    add service ITimeService; add inject to ShoppingCart view; add ShoppingCart view;

---
+++ ViewTask/Services/ITimeService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ViewTask.Services
+{
+    public interface ITimeService
+    {
+        public DateTime GetTimeForTomorrow();
+    }
+}
