6fc07459724bc4e7f3f0118d22aff1b9998e373e
alaketych <alaketych@ukr.net>
2021-02-25 22:10:02 +0200

    saving orders

---
+++ ViewTask/Models/Repository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ViewTask.Models {
+	public static class Repository {
+		static List<ShoppingOrder> responses = new List<ShoppingOrder>();
+
+		public static IEnumerable<ShoppingOrder> Responses 
+		{
+			get { return responses; }
+		}
+
+		public static void AddResponse(ShoppingOrder response) 
+		{
+			responses.Add(response);
+		}
+	}
+}
