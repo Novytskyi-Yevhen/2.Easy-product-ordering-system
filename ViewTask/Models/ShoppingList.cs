56148559f79d9a29efa331e1e3e82608cfadd1d8
alaketych <alaketych@ukr.net>
2021-02-25 22:10:31 +0200

    Shopping order

---
+++ ViewTask/Models/ShoppingList.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace ViewTask.Models {
+	public class ShoppingOrder 
+	{
+		[Required(ErrorMessage = "Please enter your name")]
+		public string Name { get; set; }
+
+		[Required(ErrorMessage = "Please enter your email adress")]
+		[RegularExpression(".+\\@.+\\..+",
+			ErrorMessage = "Please enter a valid email address")]
+		public string Address { get; set; }
+
+		[Required(ErrorMessage = "Please select one of malls")]
+		public string Mall { get; set; }
+
+		[Required(ErrorMessage = "Please select a date")]
+		public DateTime? ShoppingDate { get; set; }
+	}
+}
