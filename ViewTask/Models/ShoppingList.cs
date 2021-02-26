fc22d880cc80c2d2f80f9f37072a7a8a67f7e16e
alaketych <alaketych@ukr.net>
2021-02-26 15:56:01 +0200

    refactor some props

--- ViewTask/Models/ShoppingList.cs
+++ ViewTask/Models/ShoppingList.cs
@@ -11,14 +11,11 @@ namespace ViewTask.Models {
 		public string Name { get; set; }
 
 		[Required(ErrorMessage = "Please enter your email adress")]
-		[RegularExpression(".+\\@.+\\..+",
-			ErrorMessage = "Please enter a valid email address")]
 		public string Address { get; set; }
 
 		[Required(ErrorMessage = "Please select one of malls")]
 		public string Mall { get; set; }
 
-		[Required(ErrorMessage = "Please select a date")]
 		public DateTime? ShoppingDate { get; set; }
 	}
 }
